fd5d60ffc5ab047aa519252086d30854b93d0ab9
missi0415 <missi0415@gmail.com>
2021-02-26 21:25:35 +0900

    omikuji

--- JabaScriptBasics/js/main.js
+++ JabaScriptBasics/js/main.js
@@ -1,16 +1,10 @@
 'use strict';
-{
-  
-  let i = 0;
-  function showTime() {
-    console.log(new Date());
-    //指定した時間の後に1回だけ処理を実行する命令
-    const timeoutId = setTimeout(showTime, 1000);
-    i++;
-    if (i > 2) {
-      clearTimeout(timeoutId);
-    }
-  }
-  showTime();
 
+{
+  document.querySelector('ul').addEventListener('click', (e)=>{
+    if (e.target.nodeName === "LI"){
+      e.target.classList.toggle('done');
+    }
+  });
+    
 }
